80e4d3a6efcdfd51de5436984c3c2231595a7f44
Flamenccio <kenjiromai333@gmail.com>
2025-07-21 23:40:51 -0400

    add stamina drain

--- game_manager.gd
+++ game_manager.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
 signal on_game_over()
+signal stamina_drained(amount: float)
+
+## Base amount of stamina drained per second, from 0.0 - 1.0
+@export var _base_stamina_drain_amount: float = 0.0
+var _stamina_drain_amount: float = 0.0
+
+func _ready() -> void:
+	_stamina_drain_amount = _base_stamina_drain_amount
+
 
 func game_over() -> void:
 	get_tree().paused = true
 	on_game_over.emit()
+
+
+func _process(delta: float) -> void:
+	stamina_drained.emit(_stamina_drain_amount * delta * -1)
