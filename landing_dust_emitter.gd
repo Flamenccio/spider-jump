f4d55211f93ba2ff991afc342ba87f4c8b5f5f32
Flamenccio <kenjiromai333@gmail.com>
2025-07-23 22:21:13 -0400

    add jump particles

--- landing_dust_emitter.gd
+++ landing_dust_emitter.gd
@@ -3,8 +3,13 @@ extends Node
 @export var _player: Node2D
 @export var dust_scene: PackedScene
 
+@export var jump_dust_scene: PackedScene
+
 func spawn_dust(normal: Vector2) -> void:
 	var angle_rad = (Vector2.UP).angle_to(normal)
 	TheGlobalSpawner.instantiate_scene(dust_scene, _player.global_position, angle_rad)
-	print('asdf')
+
+
+func spawn_jump_dust() -> void:
+	TheGlobalSpawner.instantiate_scene(jump_dust_scene, _player.global_position, _player.rotation)
 
