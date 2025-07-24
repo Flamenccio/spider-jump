14c20a108fbf9283a1bcfc3f7845b5f8511d6ee5
Flamenccio <kenjiromai333@gmail.com>
2025-07-24 19:56:32 -0400

    add suction to floor when idle

--- states/idle.gd
+++ states/idle.gd
@@ -4,10 +4,17 @@ extends BehaviorState
 @export var _gravity_scale: float = 1.0
 @export var _player: CharacterBody2D
 
+var _floor_suction_force: float = 50.0
 signal player_entered_idle()
 
 func enter_state() -> void:
-	_player.velocity = Vector2.ZERO
+	var down_vector = Vector2.from_angle(_player.rotation - (3 * PI / 2.0))
+	print('down: ', down_vector)
+	_player.velocity = down_vector * _floor_suction_force
 	set_property('jump', false)
 	_animator.play('idle')
 	player_entered_idle.emit()
+
+
+func tick_state(delta: float) -> void:
+	_player.move_and_slide()
