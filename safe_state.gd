6639f5e01963165a733565366314673bc6dbb3f8
Flamenccio <kenjiromai333@gmail.com>
2025-07-26 19:04:03 -0400

    make safe states safer

--- safe_state.gd
+++ safe_state.gd
@@ -4,11 +4,22 @@ Saves the last place the player was standing
 """
 
 @export var _player: CharacterBody2D
+@export var _danger_shapecast: ShapeCastCheck
+@export var _ground_shapecast: ShapeCastCheck
 var _position: Vector2
 var _rotation: float
 
+const _MAX_SHAPECAST_RESULTS = 1
+
 func _save_state() -> void:
 	var rounded_position = Vector2(roundf(_player.global_position.x), roundf(_player.global_position.y))
+
+	# Check area for danger (spikes)
+	var results := _danger_shapecast.intersect_shape(Vector2.ZERO, _MAX_SHAPECAST_RESULTS, false)
+	if results.size() > 0:
+		print('danger!!')
+		return
+
 	_position = rounded_position
 	_rotation = _player.rotation
 
--- states/idle.gd
+++ states/idle.gd
@@ -5,16 +5,16 @@ extends BehaviorState
 @export var _player: CharacterBody2D
 
 var _floor_suction_force: float = 50.0
+var _down_vector: Vector2
 signal player_entered_idle()
 
 func enter_state() -> void:
-	var down_vector = Vector2.from_angle(_player.rotation - (3 * PI / 2.0))
-	print('down: ', down_vector)
-	_player.velocity = down_vector * _floor_suction_force
+	_down_vector = Vector2.from_angle(_player.rotation - (3 * PI / 2.0))
 	set_property('jump', false)
 	_animator.play('idle')
 	player_entered_idle.emit()
 
 
 func tick_state(delta: float) -> void:
-	_player.move_and_slide()
+	_player.move_and_collide(_down_vector * _floor_suction_force)
+
