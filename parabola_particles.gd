d1ea0c034e6dfe86f8c1b21bba8e5aacc0512b7f
Flamenccio <kenjiromai333@gmail.com>
2025-07-27 20:28:34 -0400

    replace line trajectory path with sprites

--- parabola_particles.gd
+++ parabola_particles.gd
@@ -1,50 +1,82 @@
 extends Node2D
 
 @export var _origin: Node2D
-@export var _line: Line2D
-@export var _max_points: int = 10
+@export var _particle_prefab: PackedScene
+@export_range(0, 100) var _particle_count: int = 10
+
+var inactive_particles: Array[TrajectoryParticle]
+var active_particles: Array[TrajectoryParticle]
 
 var velocity: Vector2
 var acceleration: Vector2
-var active: bool = false
-var step = 0.05
+
+var activation_timer: Timer = Timer.new()
 
 const _HALF = 0.50
+const _ACTIVATION_FREQUENCY = 0.20
 
 func _ready() -> void:
+
+	# Instantiate prefabs
+	for i in range(_particle_count):
+		var instance = _particle_prefab.instantiate()
+		if instance is not TrajectoryParticle:
+			printerr('parabola particles: given prefab is not of type TrajectoryParticle')
+			instance.queue_free()
+			return
+		var particle = instance as TrajectoryParticle
+		particle.return_to_pool = _return_particle
+		particle.point_on_path = path
+		inactive_particles.push_back(particle)
+		add_child(particle)
+	
+	# Set up timer
+	activation_timer.wait_time = _ACTIVATION_FREQUENCY
+	activation_timer.timeout.connect(_activate_next_particle)
+	add_child(activation_timer)
+
+	# Deactivate all
 	deactivate_trajectory()
 
 
 func path(t: float) -> Vector2:
 	var x = velocity.x * t + _HALF * acceleration.x * pow(t, 2.0)
 	var y = velocity.y * t + _HALF * acceleration.y * pow(t, 2.0)
-	return Vector2(x, y)
-
-
-func _get_points() -> Array[Vector2]:
-	var points: Array[Vector2] = []
-	var t = 0.0
-	var count = 0;
-	while count < _max_points:
-		count += 1
-		points.append(path(t).rotated(-_origin.rotation))
-		t += step
-	return points
-
-
-func show_trajectory(velocity: Vector2, acceleration: Vector2) -> void:
-	if not active: return
-	self.velocity = velocity
-	self.acceleration = acceleration
-	var points = _get_points()
-	_line.points = points
+	return Vector2(x, y) + _origin.global_position
 
 
 func activate_trajectory() -> void:
-	_line.show()
-	active = true
+	_activate_next_particle()
+	activation_timer.start()
 
 
 func deactivate_trajectory() -> void:
-	_line.hide()
-	active = false
+	activation_timer.stop()
+	for i in range(active_particles.size()):
+		var particle = active_particles.pop_front() as TrajectoryParticle
+		particle.deactivate_particle()
+		inactive_particles.push_back(particle)
+
+
+func _activate_next_particle() -> void:
+	var particle = inactive_particles.pop_front() as TrajectoryParticle
+	if particle == null:
+		print('parabola particles: ran out of particles!')
+		return
+	particle.global_position = _origin.global_position
+	particle.activate_particle()
+	active_particles.push_back(particle)
+
+
+func _update_trajectory(new_velocity: Vector2, new_acceleration: Vector2) -> void:
+	velocity = new_velocity
+	acceleration = new_acceleration
+
+
+func _return_particle(particle: TrajectoryParticle) -> void:
+	var index = active_particles.find(particle)
+	if index < 0:
+		return
+	var found = active_particles.pop_at(index)
+	inactive_particles.push_back(found)
+
--- trajectory_particle.gd
+++ trajectory_particle.gd
@@ -1,4 +1,36 @@
 class_name TrajectoryParticle
 extends AnimatedSprite2D
 
-@export var shader_mat: ShaderMaterial
+var active: bool = false
+
+## Accepts float `t` as a parameter and returns a Vector2
+var point_on_path: Callable
+
+## Accepts a `TrajectoryParticle` as a parameter and has no return value
+var return_to_pool: Callable
+
+# When `t` reaches this value, the particle will deactivate
+const MAX_T: float = 1.0
+var t: float = 0.0
+
+func activate_particle() -> void:
+	active = true
+	play(&"default")
+	show()
+
+
+func deactivate_particle() -> void:
+	active = false
+	stop()
+	t = 0.0
+	return_to_pool.call(self as TrajectoryParticle)
+	hide()
+
+
+func _process(delta: float) -> void:
+	if not active: return
+	if t >= MAX_T:
+		deactivate_particle()
+	var point = point_on_path.call(t) as Vector2
+	global_position = point
+	t += delta
