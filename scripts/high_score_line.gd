35b08d7f4ab0a1e347f5524fca8d1cdd88c86749
Flamenccio <kenjiromai333@gmail.com>
2025-08-01 15:31:51 -0400

    make line showing high score in game

--- scripts/high_score_line.gd
+++ scripts/high_score_line.gd
@@ -2,16 +2,30 @@ extends Node2D
 
 signal player_entered()
 @export var _particle_emitter: SpriteParticleEmitter
+@export var _high_score_saver: Node
 
 var _death_timer: Timer = Timer.new()
 
 const _DEATH_TIMER_DURATION = 3.0
+const _PIXEL_PER_POINT = 8
 
 func _ready() -> void:
 	_death_timer.one_shot = true
 	_death_timer.wait_time = _DEATH_TIMER_DURATION
 	_death_timer.timeout.connect(func(): queue_free())
 	add_child(_death_timer)
+
+
+func _on_stats_saver_ready() -> void:
+	var high_score = _high_score_saver.get_high_score()
+
+	# Move to high score elevation
+	if high_score == 0 or high_score == null:
+		# Do not show if there is no high score
+		queue_free()
+		return
+
+	global_position = Vector2(0.0, -1 * high_score * _PIXEL_PER_POINT)
 
 
 func _on_player_entered() -> void:
