2f4bf9023c74de18843b97f01adcd612f7e5e1f0
Flamenccio <kenjiromai333@gmail.com>
2025-08-01 22:18:51 -0400

    add level up text

--- scripts/level_up_platform.gd
+++ scripts/level_up_platform.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
+@export var _particle_emitter: SpriteParticleEmitter
 signal platform_triggered()
 
 func _on_player_entered() -> void:
+	_particle_emitter.spawn_particle('level_up_text', global_position)
 	platform_triggered.emit()
