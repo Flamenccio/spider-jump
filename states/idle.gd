94194f111041b380d6bb6cb28c1fcd89429a3a84
Flamenccio <kenjiromai333@gmail.com>
2025-08-27 14:33:01 -0400

    improve slip prevention

--- states/idle.gd
+++ states/idle.gd
@@ -9,34 +9,37 @@ signal normal_recheck_requested()
 
 var _floor_suction_force: float = 0.5
 var _down_vector: Vector2
-# Used to prevent 'slipping' when the player lands on a corner
-var _recheck_normal_timer: Timer = Timer.new()
 
-# In seconds
-const _RECHECK_NORMAL_FREQUENCY = 0.20
+# Used for motion detection
+var _last_position: Vector2
 
-func _ready() -> void:
-	_recheck_normal_timer.wait_time = _RECHECK_NORMAL_FREQUENCY
-	_recheck_normal_timer.timeout.connect(func(): normal_recheck_requested.emit())
-	add_child(_recheck_normal_timer)
-
+# Lower values mean higher precision
+const _MOTION_DETECTION_PRECISION = 0.1
 
 func enter_state() -> void:
 	_player.velocity = Vector2.ZERO
+	_last_position = _player.global_position
 	_down_vector = Vector2.from_angle(_player.rotation - (3 * PI / 2.0))
 	set_param('jump', false)
 	_animator.play_branch_animation('idle')
 	player_entered_idle.emit()
-	_recheck_normal_timer.start()
-
-
-func exit_state() -> void:
-	_recheck_normal_timer.stop()
 
 
 func update_state(delta: float) -> void:
+
 	_player.move_and_collide(_down_vector * _floor_suction_force)
+
+	if _motion_detected():
+		normal_recheck_requested.emit()
+		_last_position = _player.global_position
 
 
 func _normal_updated(normal: Vector2) -> void:
 	_down_vector = Vector2.from_angle(_player.rotation - (3 * PI / 2.0))
+
+
+func _motion_detected() -> bool:
+	var snapped_last_position = _last_position.snappedf(_MOTION_DETECTION_PRECISION)
+	var snapped_global_position = _player.global_position.snappedf(_MOTION_DETECTION_PRECISION)
+	return snapped_last_position != snapped_global_position
+
