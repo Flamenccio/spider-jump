80ce6746ce67ea2c547ff15ec7828d36de96986e
Flamenccio <kenjiromai333@gmail.com>
2025-09-23 14:47:06 -0400

    refactor hint translations

--- scripts/player_hints.gd
+++ scripts/player_hints.gd
@@ -2,13 +2,15 @@ extends Node
 
 signal give_hint(hint: String)
 
+const _HINT_PREFIX = "hint."
+
 func _ready() -> void:
     PlayerEventBus.powerup_started.connect(_on_powerup_started)
     PlayerEventBus.powerup_ended.connect(_on_powerup_ended)
 
 
 func _on_powerup_started(powerup: String) -> void:
-    give_hint.emit(StringTableServer.get_string("powerup_hints", "hint." + powerup))
+    give_hint.emit(tr(_HINT_PREFIX + powerup))
 
 
 func _on_powerup_ended(_powerup: String) -> void:
