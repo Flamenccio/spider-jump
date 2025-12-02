dfc5fc9614b603c8f4117861cb6bfee6e921dcdf
Flamenccio <kenjiromai333@gmail.com>
2025-12-01 23:08:03 -0500

    add repeat tutorial option

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,17 +1,35 @@
 extends Control
 
 const ARCADE_GAME_UID = "uid://c3vqs430qpt8w"
-var _queued_game_mode := ""
+const TUTORIAL_SCREEN_UID = "uid://bh66jtkoetxiq"
+const PLAYER_SETTINGS_PATH = "res://resources/settings/player_settings.res"
 
+var _queued_game_mode := ""
 
 func _queue_load_arcade() -> void:
 	if _queued_game_mode != "":
 		return
+
+	var settings = SettingsService.get_settings()
+	if settings.get("repeat_tutorial") or not settings.get("tutorial_played"):
+		_queued_game_mode = "tutorial"
+		return
+
 	_queued_game_mode = "arcade"
 
 
 func _load_arcade() -> void:
-	var path = ResourceUID.uid_to_path(ARCADE_GAME_UID)
+	_switch_scene(ARCADE_GAME_UID)
+
+
+func _load_tutorial() -> void:
+	SettingsService.update_setting("tutorial_played", true)
+	SettingsService.write_settings()
+	_switch_scene(TUTORIAL_SCREEN_UID)
+
+
+func _switch_scene(uid: String) -> void:
+	var path = ResourceUID.uid_to_path(uid)
 	if get_tree().change_scene_to_file(path) != OK:
 		push_error("main menu: something went wrong!")
 
@@ -25,5 +43,7 @@ func _on_black_transition_animation_finished() -> void:
 	match _queued_game_mode:
 		"arcade":
 			_load_arcade()
+		"tutorial":
+			_load_tutorial()
 		_:
 			return
