de558ffff67e073f6fe33998b4c6ca77ef409feb
Flamenccio <kenjiromai333@gmail.com>
2025-12-17 23:40:15 -0500

    refactor camera offsets

--- scripts/entities/camera.gd
+++ scripts/entities/camera.gd
@@ -1,12 +1,9 @@
 extends Camera2D
 
-const _CAMERA_HEIGHT = 16.0
+signal message_offsetters(msg: String, value: Variant)
 
-# SCREEN SHAKING
-const _MAX_SCREEN_SHAKE_POWER = 1.0
-const _MIN_SCREEN_SHAKE_POWER = 0.0
-const _SCREEN_SHAKE_DURATION_SECONDS = 0.20
-const _SCREEN_SHAKE_MAX_INTENSITY = 5.0
+## Permanent vertical distance of camera
+const _CAMERA_HEIGHT = -16.0
 
 # DYNAMIC CAMERA SPEED
 const _MIN_SMOOTHING_SPEED = 3.0
@@ -15,27 +12,17 @@ const _MIN_PLAYER_SPEED = 0.0
 const _MAX_PLAYER_SPEED = 300.0
 const _SMOOTHING_RATIO = (_MAX_SMOOTHING_SPEED - _MIN_SMOOTHING_SPEED) / (_MAX_PLAYER_SPEED - _MIN_PLAYER_SPEED)
 
-# CAMERA PEEKING
-## Minimum normalized height of mouse on screen to trigger pull peeking.
-## This value should be between 0.0 and 1.0.
-const _PEEK_TRIGGER_MOUSE_HEIGHT = 0.8
-const _PEEK_TRIGGER_PLAYER_DISTANCE = 54.0
-const _PEEK_MAX_DISTANCE = 24.0
-
 # POWERUP OFFSETS
 const _DEFAULT_POWERUP_HEIGHT_OFFSET = -32.0
 
-var _screen_shake_timer := Timer.new()
-var _screen_shake_intensity := 0.0
-var _player_is_pulling := false
-
-## Pull peeking camera offset
-var _peek_offset := Vector2.ZERO
+# OFFSETS ---
 
 ## Offset used for camera shifts during certain powerups
 var _powerup_offset := Vector2.ZERO
 
-var _screen_shake_offset := Vector2.ZERO
+# ---
+
+var _offsetters: Array[CameraOffsetter]
 
 @export var _follow_target: Node2D
 
@@ -47,16 +34,28 @@ var _screen_shake_offset := Vector2.ZERO
 func _ready() -> void:
 
 	GameConstants.main_camera = self
-
-	_screen_shake_timer.timeout.connect(end_screen_shake)
-	_screen_shake_timer.one_shot = true
-	add_child(_screen_shake_timer)
-
 	PlayerEventBus.powerup_started.connect(_on_powerup_started)
 	PlayerEventBus.powerup_ended.connect(_on_powerup_ended)
 
+	# Initialize all camera offsetters
+	for c in get_children():
+		if c is CameraOffsetter:
+			c.camera = self
+			c.follow_target = _follow_target
+			c.world_to_screen_point = world_to_screen_point
+			c.screen_to_world_point = screen_to_world_point
+			message_offsetters.connect(c._on_receive_message)
+			_offsetters.append(c)
 
-func _process(delta: float) -> void:
+
+func _compile_offsets() -> void:
+	var running_total = Vector2.ZERO
+	for co in _offsetters:
+		running_total += co.get_offset()
+	offset = running_total
+
+
+func _process(_delta: float) -> void:
 
 	# Debug movement
 	if _debug:
@@ -65,29 +64,19 @@ func _process(delta: float) -> void:
 		_movement()
 		_set_dynamic_smoothing_speed()
 
-	if _player_is_pulling:
-		_peek_mouse()
-	else:
-		_peek_offset = _peek_offset.lerp(Vector2.ZERO, 0.05)
+	_compile_offsets()
 
 
-func _physics_process(delta: float) -> void:
-	if _screen_shake_intensity > 0:
-		var point = _get_point_in_circle()
-		_screen_shake_offset = point * _screen_shake_intensity
-	offset = _screen_shake_offset + _peek_offset + _powerup_offset
+## Make the camera shake a bit, with [code]power[/code] affecting the intensity
+## of the screen shake. This value has a range from 0 to 1.
+func screen_shake(power: float) -> void:
+	message_offsetters.emit("start_screen_shake", power)
 
 
-func _debug_movement() -> void:
-	if _follow_target != null:
-		global_position = Vector2(global_position.x, _follow_target.global_position.y)
-
-
-func _movement() -> void:
-	if _follow_target != null:
-		var new_vertical = minf(global_position.y, _follow_target.global_position.y - _CAMERA_HEIGHT)
-		var new_position = Vector2(global_position.x, new_vertical)
-		global_position = new_position
+func set_powerup_offset(p_offset: float, duration := 0.3) -> void:
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "_powerup_offset", Vector2(0, p_offset), duration) 
 
 
 func screen_to_world_point(screen_point: Vector2) -> Vector2:
@@ -98,18 +87,18 @@ func world_to_screen_point(world_point: Vector2) -> Vector2:
 	return get_canvas_transform() * world_point
 
 
-## Make the camera shake a bit, with [code]power[/code] affecting the intensity
-## of the screen shake. This value has a range from 0 to 1.
-func screen_shake(power: float) -> void:
-	_screen_shake_timer.stop()
-	power = clampf(power, _MIN_SCREEN_SHAKE_POWER, _MAX_SCREEN_SHAKE_POWER)
-	_screen_shake_intensity = power * _SCREEN_SHAKE_MAX_INTENSITY
-	_screen_shake_timer.start(_SCREEN_SHAKE_DURATION_SECONDS)
+# MOVEMENT ---
+
+func _debug_movement() -> void:
+	if _follow_target != null:
+		global_position = Vector2(global_position.x, _follow_target.global_position.y)
 
 
-func _get_point_in_circle() -> Vector2:
-	var random_angle = randf() * 2.0 * PI
-	return Vector2(cos(random_angle), sin(random_angle))
+func _movement() -> void:
+	if _follow_target != null:
+		var new_vertical = minf(global_position.y, _follow_target.global_position.y + _CAMERA_HEIGHT)
+		var new_position = Vector2(global_position.x, new_vertical)
+		global_position = new_position
 
 
 func _set_dynamic_smoothing_speed() -> void:
@@ -123,61 +112,17 @@ func _set_dynamic_smoothing_speed() -> void:
 	position_smoothing_speed = lerpf(position_smoothing_speed, target_smoothing, 0.05)
 
 
-func end_screen_shake() -> void:
-	_screen_shake_intensity = 0.0
-	_screen_shake_offset = Vector2.ZERO
-	_screen_shake_timer.stop()
-
-
-func _on_pull_pressed() -> void:
-	_player_is_pulling = true
-
-
-func _on_pull_released() -> void:
-	_player_is_pulling = false
-
-
-# Move the camera offset down if the mouse is
-# close to the bottom edge and if the player is
-# below the camera (not including offset) a certain distance
-func _peek_mouse() -> void:
-
-	var viewport_mouse_position = get_viewport().get_mouse_position()
-	var normalized_mouse_position = viewport_mouse_position / get_viewport_rect().size
-	var camera_player_height_difference = global_position.y - _follow_target.global_position.y
-
-	var mouse_close = normalized_mouse_position.y >= _PEEK_TRIGGER_MOUSE_HEIGHT
-	var player_close = camera_player_height_difference <= -_PEEK_TRIGGER_PLAYER_DISTANCE
-
-	if mouse_close and player_close:
-		_peek_offset = Vector2(0.0, lerpf(_peek_offset.y, _PEEK_MAX_DISTANCE + absf(_powerup_offset.y), 0.02)) 
-
-
-func smooth_zoom(zoom_level: float) -> void:
-	var z = Vector2(zoom_level, zoom_level)
-	var tween = create_tween()
-	tween.tween_property(self, "zoom", z, 0.5)
-	tween.set_trans(Tween.TRANS_SINE)
-
-
 func _on_powerup_started(powerup: String) -> void:
 	match powerup:
 		ItemIds.HOVERFLY_POWERUP:
-			set_follow_offset(_DEFAULT_POWERUP_HEIGHT_OFFSET)
+			set_powerup_offset(_DEFAULT_POWERUP_HEIGHT_OFFSET)
 		ItemIds.HOPPERPOP_POWERUP:
-			set_follow_offset(_DEFAULT_POWERUP_HEIGHT_OFFSET)
+			set_powerup_offset(_DEFAULT_POWERUP_HEIGHT_OFFSET)
 		ItemIds.ANTIBUG_POWERUP:
-			set_follow_offset(_DEFAULT_POWERUP_HEIGHT_OFFSET)
+			set_powerup_offset(_DEFAULT_POWERUP_HEIGHT_OFFSET)
 		_:
 			return
 
 
 func _on_powerup_ended(_powerup: String) -> void:
-	set_follow_offset(0.0, 3.3)
-
-
-func set_follow_offset(offset: float, duration := 0.3) -> void:
-	var tween := create_tween()
-	tween.set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self, "_powerup_offset", Vector2(0, offset), duration) 
-
+	set_powerup_offset(0.0, 3.3)
